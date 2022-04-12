4e139e1f5b3ca03e4993b0c58580551aa8a3e21c
David Perez <43676929+Avi1235@users.noreply.github.com>
2022-04-12 14:16:22 -0600

    release.Url Typo

--- .github/workflows/addPluginReleaseToRepo.go
+++ .github/workflows/addPluginReleaseToRepo.go
@@ -70,7 +70,7 @@ func main() {
 func addReleaseToPlugins(releaseEvent PluginReleaseEvent, existingPlugins []Plugin) []Plugin {
     releasedPlugin := releaseEvent.Released
     release := releasedPlugin.Releases[0]
-    release.Url = "https://github.com/" + releaseEvent.ReleaseRepo + "/" + releaseEvent.ReleaseRepo + "/releases/download/" + release.Version + "/" + releaseEvent.Repo + "-" + release.Version + ".zip"
+    release.Url = "https://github.com/" + releaseEvent.ReleaseOrg + "/" + releaseEvent.ReleaseRepo + "/releases/download/" + release.Version + "/" + releaseEvent.Repo + "-" + release.Version + ".zip"
     if strings.HasPrefix(release.Version,"v") {
         // the plugins version is supplied with a v from the bundler, but fails when update manager compares versions
         release.Version = release.Version[1:]
